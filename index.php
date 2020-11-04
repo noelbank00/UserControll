617d2c46d334babcbae7231ef4ac38fcdd336a9f
noelbank <bank.noel2000@gmail.com>
2020-11-04 17:19:25 +0100

    Alap

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="hu">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>AdminLTE 3 | Blank Page</title>
+
+		<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+		<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+		<link rel="stylesheet" href="dist/css/adminlte.min.css">
+
+		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+	</head>
+	<body class="hold-transition sidebar-mini">
+		<!-- Site wrapper -->
+		<div class="wrapper">
+			<!-- Navbar -->
+			<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+				<!-- Left navbar links -->
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+					</li>
+					<li class="nav-item d-none d-sm-inline-block">
+						<a href="" class="nav-link">Home</a>
+					</li>
+				</ul>
+
+				<!-- Right navbar links -->
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="/logout">Logout</a>
+					</li>
+				</ul>
+			</nav>
+			<!-- /.navbar -->
+
+			<!-- Main Sidebar Container -->
+			<aside class="main-sidebar sidebar-dark-primary elevation-4">
+				<!-- Brand Logo -->
+				<a href="" class="brand-link">
+					<img src="dist/img/AdminLTELogo.png" alt="UserControl" class="brand-image img-circle elevation-3" style="opacity: .8">
+					<span class="brand-text font-weight-light">UserControl</span>
+				</a>
+
+				<!-- Sidebar -->
+				<div class="sidebar">
+					<!-- Sidebar user (optional) -->
+					<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+						<div class="image">
+							<img src="dist/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
+						</div>
+						<div class="info">
+							<a href="#" class="d-block">Username</a>
+						</div>
+					</div>
+
+					<!-- Sidebar Menu -->
+					<nav class="mt-2">
+						<ul class="nav nav-pills nav-sidebar flex-column nav-child-indent nav-flat" data-widget="treeview" role="menu" data-accordion="false">
+							<!-- Add icons to the links using the .nav-icon class
+									 with font-awesome or any other icon font library -->
+							<li class="nav-item">
+								<a href="" class="nav-link">
+									<i class="nav-icon fas fa-home"></i>
+									<p>
+										Home
+										<span class="right badge badge-danger">New</span>
+									</p>
+								</a>
+							</li>
+							<li class="nav-header">MULTI LEVEL EXAMPLE</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link">
+									<i class="fas fa-circle nav-icon"></i>
+									<p>Level 1</p>
+								</a>
+							</li>
+							<li class="nav-item has-treeview">
+								<a href="#" class="nav-link">
+									<i class="nav-icon fas fa-circle"></i>
+									<p>
+										Level 1
+										<i class="right fas fa-angle-left"></i>
+									</p>
+								</a>
+								<ul class="nav nav-treeview">
+									<li class="nav-item">
+										<a href="#" class="nav-link">
+											<i class="far fa-circle nav-icon"></i>
+											<p>Level 2</p>
+										</a>
+									</li>
+									<li class="nav-item has-treeview">
+										<a href="#" class="nav-link">
+											<i class="far fa-circle nav-icon"></i>
+											<p>
+												Level 2
+												<i class="right fas fa-angle-left"></i>
+											</p>
+										</a>
+										<ul class="nav nav-treeview">
+											<li class="nav-item">
+												<a href="#" class="nav-link">
+													<i class="far fa-dot-circle nav-icon"></i>
+													<p>Level 3</p>
+												</a>
+											</li>
+											<li class="nav-item">
+												<a href="#" class="nav-link">
+													<i class="far fa-dot-circle nav-icon"></i>
+													<p>Level 3</p>
+												</a>
+											</li>
+											<li class="nav-item">
+												<a href="#" class="nav-link">
+													<i class="far fa-dot-circle nav-icon"></i>
+													<p>Level 3</p>
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a href="#" class="nav-link">
+											<i class="far fa-circle nav-icon"></i>
+											<p>Level 2</p>
+										</a>
+									</li>
+								</ul>
+							</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link">
+									<i class="fas fa-circle nav-icon"></i>
+									<p>Level 1</p>
+								</a>
+							</li>
+							<li class="nav-header">LABELS</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link">
+									<i class="nav-icon far fa-circle text-danger"></i>
+									<p class="text">Important</p>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link">
+									<i class="nav-icon far fa-circle text-warning"></i>
+									<p>Warning</p>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link">
+									<i class="nav-icon far fa-circle text-info"></i>
+									<p>Informational</p>
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</aside>
+
+			<div class="content-wrapper">
+				<section class="content-header">
+					<div class="container-fluid">
+						<div class="row mb-2">
+							<div class="col-sm-6">
+								<h1>Blank Page</h1>
+							</div>
+							<div class="col-sm-6">
+								<ol class="breadcrumb float-sm-right">
+									<li class="breadcrumb-item"><a href="#">Home</a></li>
+									<li class="breadcrumb-item active">Blank Page</li>
+								</ol>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- Main content -->
+				<section class="content">
+
+					<div class="card">
+						<div class="card-header">
+							<h3 class="card-title">Title</h3>
+
+							<div class="card-tools">
+								<button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+									<i class="fas fa-minus"></i></button>
+								<button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+									<i class="fas fa-times"></i></button>
+							</div>
+						</div>
+						<div class="card-body">
+							Start creating your amazing application!
+						</div>
+						<div class="card-footer">
+							Footer
+						</div>
+					</div>
+
+				</section>
+			</div>
+
+			<footer class="main-footer">
+				<div class="float-right d-none d-sm-block">
+					<b>Version</b> 3.0.5
+				</div>
+				<strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
+				reserved.
+			</footer>
+
+			
+			<aside class="control-sidebar control-sidebar-dark">
+				<!-- Control sidebar content goes here -->
+			</aside>
+		</div>
+
+	<script src="plugins/jquery/jquery.min.js"></script>
+	<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="dist/js/adminlte.min.js"></script>
+	<script src="dist/js/script.js"></script>
+	</body>
+</html>
